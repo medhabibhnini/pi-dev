1e1b638af9b451675c76d9e4ce576b6396741b63
medhabibhnini <60883388+medhabibhnini@users.noreply.github.com>
2020-02-27 10:05:50 +0100

    rectification

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/notlogedF", name="home")
+     * @Route("/", name="home")
      */
     public function indexAction(Request $request)
     {
